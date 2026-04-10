bbd8c7f3f02bb4c0a501571f5ebdd17858b615a7
Nikhil-Chatse <nikhilchatse143@gmail.com>
2026-04-10 14:35:39 +0530

    settings

--- online_doctor_appointment/online_doctor_appointment/settings.py
+++ online_doctor_appointment/online_doctor_appointment/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 from django.contrib.messages import constants as messages
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -135,7 +136,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
